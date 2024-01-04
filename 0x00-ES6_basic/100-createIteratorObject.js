891d65862682a885a36ca9522d382c6d54d7fb25
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2024-01-04 01:31:56 +0000

    Iterate report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+  const employeeList = [];
+  /* eslint-disable */
+  for (const [department, employees] of Object.entries(report.allEmployees)) {
+    for (const employee of employees) {
+      employeeList.push(employee);
+    }
+  }
+  return employeeList;
+}
